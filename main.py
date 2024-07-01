aed6c20cbcbbd457153089e59590991105d99b73
ngjunlong9651 <42053960+ngjunlong9651@users.noreply.github.com>
2024-07-01 23:44:03 +0800

    fixing formatting error 4

--- main.py
+++ main.py
@@ -1,29 +1,15 @@
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update, WebAppInfo, ParseMode
+from telegram.ext import Application, CallbackQueryHandler, CommandHandler, ContextTypes, ConversationHandler
+from datetime import datetime
+import requests
 import os
 import logging
 from dotenv import load_dotenv
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update, WebAppInfo
-from telegram.ext import (
-    Application,
-    CallbackQueryHandler,
-    CommandHandler,
-    ContextTypes,
-    ConversationHandler,
-)
-from telegram.constants import ParseMode
-
-from datetime import datetime
-import requests
-
-
-
-# Load environment variables
-load_dotenv()
 
 # Set up logging to a file
 logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s - %(message)s",
-)
+    level=logging.INFO
+    )
 
 logger = logging.getLogger(__name__)
 
@@ -63,23 +49,22 @@ async def reminder(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
             logger.error("Prod URL is not set in the environment variables.")
             await update.message.reply_text("PROD_URL is not set in the environment variables.")
             return
-        
+
         response = requests.get(f"{prod_url}/api/orders/?populate=*", headers={
             'Authorization': f"Bearer {os.getenv('API_TOKEN')}"
         })
         response.raise_for_status()
         orders = response.json().get('data', [])
-        print(orders)
         
         for order in orders:
             if order['attributes']['fulfilmentEnd'] is None:
                 order['attributes']['fulfilmentEnd'] = order['attributes']['fulfilmentStart']
-        
+
         active_orders = [
             order for order in orders
             if order['attributes']['order_status']['data']['attributes']['orderStatus'] not in ['Completed', 'Cancelled']
         ]
-        
+
         due_today_orders = [
             order for order in active_orders
             if (
@@ -87,7 +72,7 @@ async def reminder(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
                 (datetime.strptime(order['attributes']['fulfilmentEnd'], "%Y-%m-%dT%H:%M:%S.%fZ").date())
             ) 
         ]
-         
+
         if due_today_orders:
             order_message = f"*📋 Orders due today: {today}*\n\n"
             for order in due_today_orders:
@@ -99,17 +84,16 @@ async def reminder(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
                 order_message += f"*Contact Number:* `{attributes['customerContact']}`\n"
                 order_message += "*Products:*\n"
                 for product in attributes['orderProducts']:
+                    product_name = product['name'].replace("-", r"\-")
                     order_message += (
-                        f"  \\- {product['name']} (SKU: {product['sku']}, "
-                        f"Brand: {product['brand']}, "
-                        f"Category: {product['category']}, "
-                        f"Quantity: {product['quantity']}, "
-                        f"Price: \\${product['price']})\n"
+                        f"  \- *{product_name}* \(SKU: `{product['sku']}`, "
+                        f"Brand: `{product['brand']}`, "
+                        f"Category: `{product['category']}`, "
+                        f"Quantity: `{product['quantity']}`, "
+                        f"Price: `${product['price']}`\)\n"
                     )
                 order_message += f"*Remarks:* `{attributes['remarks']}`\n\n"
             await update.message.reply_text(order_message, parse_mode=ParseMode.MARKDOWN_V2)
-            ## hopefully this fixes it
-            
         else:
             await update.message.reply_text("No orders due today.")
     except Exception as e:
@@ -117,6 +101,7 @@ async def reminder(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
         await update.message.reply_text("An error occurred while fetching orders.")
     
     return START_ROUTES
+
 
 
                 
